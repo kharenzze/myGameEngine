e4573f55d82334929c2740277fd61ae97687c262
kharenzze <str237@gmail.com>
2017-12-21 00:41:48 +0100

    Added main.cpp and cmake

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Paolo on 21/12/2017.
+//
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+    cout << "lalala";
+    return 0;
+}
+
