7b7a2d7af93236778fd922089a6359aab4c47fc6
kharenzze <paolo@appfluence.com>
2017-12-24 19:01:05 +0100

    Changed background color

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ void handleInput(GLFWwindow* window) {
 }
 
 void render() {
-    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    glClearColor(.2f, .2f, .7f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 }
 
