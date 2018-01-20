710597aa18807a42f38df075971aa11b67503a59
kharenzze <str237@gmail.com>
2018-01-20 21:08:59 +0100

    Added Utils.h

---
+++ include/Utils.h
@@ -0,0 +1,14 @@
+//
+// Created by Paolo on 20/01/2018.
+//
+
+#ifndef MYGAMEENGINE_UTILS_H
+#define MYGAMEENGINE_UTILS_H
+
+#include <GLFW/glfw3.h>
+
+float inline _glfwGetTimeFloat() {
+    return glfwGetTime();
+}
+
+#endif //MYGAMEENGINE_UTILS_H
