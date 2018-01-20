82df8e778a5f5ab4d7dde3b94448fa073cb6334d
kharenzze <str237@gmail.com>
2018-01-20 21:28:47 +0100

    Added identity4 as const

--- include/Utils.h
+++ include/Utils.h
@@ -5,10 +5,13 @@
 #ifndef MYGAMEENGINE_UTILS_H
 #define MYGAMEENGINE_UTILS_H
 
+#include <glm/glm.hpp>
 #include <GLFW/glfw3.h>
 
 float inline _glfwGetTimeFloat() {
     return glfwGetTime();
 }
+
+const auto identity4 = glm::mat4(1.0f);
 
 #endif //MYGAMEENGINE_UTILS_H
