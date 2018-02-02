b26aca6e3cb58a797ce798070db7aefccd360d13
kharenzze <str237@gmail.com>
2018-02-02 15:31:04 +0100

    Using several materials

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ Camera camera(2.0f, glm::vec3(-1,2,3), 45.0f);
 Mouse mouse;
 bool firstMouse = true;
 
-GameObject cubeObject = GameObject();
+GameObject cubes[5];
 GameObject light = GameObject();
 
 void initConfiguration() {
@@ -99,11 +99,10 @@ void clearScreen() {
 
 void render() {
     clearScreen();
-
     light.drawable->draw(light.transform, camera, nullptr);
-
-    cubeObject.transform.addRotation(glm::vec3(0.0f, 0.01f, 0.0f));
-    cubeObject.drawable->draw(cubeObject.transform, camera, &light.transform, light.light);
+    for (int i = 5; i--;) {
+        cubes[i].drawable->draw(cubes[i].transform, camera, &light.transform, light.light);
+    }
 }
 
 int main (int argc, char *argv[]) {
@@ -147,26 +146,72 @@ int main (int argc, char *argv[]) {
     Shader shader_light("../shader/shader_light.vert", "../shader/shader_light.frag");
     Shader shader_flat("../shader/flat.vert", "../shader/flat.frag");
 
-    auto textDiffuse = Texture("../texture/stone_diffuse.jpg");
-    auto textSpec = Texture("../texture/stone_specular.jpg");
-
     Material matLight(&shader_light, false, false);
-    Material matCube(&shader, true, true);
-    matCube.diffuseTexture = &textDiffuse;
-    matCube.specularTexture = &textSpec;
-
     auto lightDrawable = Drawable(&matLight, &sphere);
-    auto cubeDrawable = Drawable(&matCube, &cube);
+
+    Material emerald(&shader_flat, true, true);
+    emerald.ambient = glm::vec3(0.0215f, 0.1745f, 0.0215f);
+    emerald.diffuse = glm::vec3(0.07568f, 0.61424f, 0.07568f);
+    emerald.specular = glm::vec3(0.633f, 0.727811f, 0.633f);
+    emerald.shininess = 0.6f * 128;
+    auto emeraldDrawable = Drawable(&emerald, &cube);
+
+    Material bronze(&shader_flat, true, true);
+    bronze.ambient = glm::vec3(0.2125f, 0.1275f, 0.054f);
+    bronze.diffuse = glm::vec3(0.714f, 0.4284f, 0.18144f);
+    bronze.specular = glm::vec3(0.393548f, 0.271906f, 0.166721f);
+    bronze.shininess = 0.2f * 128;
+    auto bronzeDrawable = Drawable(&bronze, &cube);
+
+    Material silver(&shader_flat, true, true);
+    silver.ambient = glm::vec3(0.19225f, 0.19225f, 0.19225f);
+    silver.diffuse = glm::vec3(0.50754f, 0.50754f, 0.50754f);
+    silver.specular = glm::vec3(0.508273f, 0.508273f, 0.508273f);
+    silver.shininess = 0.4f * 128;
+    auto silverDrawable = Drawable(&silver, &cube);
+
+    Material greenPlastic(&shader_flat, true, true);
+    greenPlastic.ambient = glm::vec3(0);
+    greenPlastic.diffuse = glm::vec3(0.1f, 0.35f, 0.1f);
+    greenPlastic.specular = glm::vec3(0.45f, 0.55f, 0.45f);
+    greenPlastic.shininess = 0.25f * 128;
+    auto greenPlasticDrawable = Drawable(&greenPlastic, &cube);
+
+    Material yellowRubber(&shader_flat, true, true);
+    yellowRubber.ambient = glm::vec3(0.05f, 0.05f, 0);
+    yellowRubber.diffuse = glm::vec3(0.5f, 0.5f, 0.4f);
+    yellowRubber.specular = glm::vec3(0.7f, 0.7f, 0.04f);
+    yellowRubber.shininess = 0.078125f * 128;
+    auto yellowRubberDrawable = Drawable(&yellowRubber, &cube);
+
 
     light.drawable = &lightDrawable;
-    light.transform.setPosition(glm::vec3(1.2f, 1.0f, 1.0f));
+    light.transform.setPosition(glm::vec3(0, 1.0f, 1.0f));
     light.transform.setScale(glm::vec3(0.3f));
     auto l = Light(glm::vec3(0.2f, 0.15f, 0.1f),
                    glm::vec3(0.7f, 0.7f, 0.7f),
                    glm::vec3(1.0f, 1.0f, 1.0f));
     light.light = &l;
 
-    cubeObject.drawable = &cubeDrawable;
+    cubes[0] = GameObject();
+    cubes[0].drawable = &emeraldDrawable;
+    cubes[0].transform.setPosition(glm::vec3(-2, 0, -2));
+
+    cubes[1] = GameObject();
+    cubes[1].drawable = &bronzeDrawable;
+    cubes[1].transform.setPosition(glm::vec3(-2, 0, 2));
+
+    cubes[2] = GameObject();
+    cubes[2].drawable = &silverDrawable;
+    cubes[2].transform.setPosition(glm::vec3(2, 0, -2));
+
+    cubes[3] = GameObject();
+    cubes[3].drawable = &greenPlasticDrawable;
+    cubes[3].transform.setPosition(glm::vec3(0, 0, 0));
+
+    cubes[4] = GameObject();
+    cubes[4].drawable = &yellowRubberDrawable;
+    cubes[4].transform.setPosition(glm::vec3(2, 0, 2));
 
     initConfiguration();
 
